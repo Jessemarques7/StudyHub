c743c1cdd79c0f9d1122c477b261abfeb6148851
Jesse Marques <jessemateus919@gmail.com>
2026-01-29 13:10:14 -0300

    second sidebar

--- app/(Pages)/notes/page.tsx
+++ app/(Pages)/notes/page.tsx
@@ -1,15 +1,5 @@
 "use client";
 
-import Editor from "@/components/notes/Editor";
-
 export default function NotePage() {
-  return (
-    <>
-      {/* Main content area */}
-
-      <div className="flex flex-row overflow-hidden relative">
-        <Editor />
-      </div>
-    </>
-  );
+  return <div></div>;
 }
